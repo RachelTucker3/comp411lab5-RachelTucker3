8cdf0ce97a41d0862c0ab33597dcba51d7c6e456
Rachel Tucker <rach1234@live.unc.edu>
2019-03-11 03:04:19 +0000

    replace this with a useful message

--- ex3.c
+++ ex3.c
@@ -6,34 +6,48 @@ char lyrics[20][60];
 int  number;
 
 void nurseryrhyme(int current) {
-                                                // print "current" number of spaces
 
-                                                // print something before the recursive call
-                                                // you need to check if the current level is 0
-                                                //   if so, print "There was an old lady..."
-                                                //   or if it is > 0, print "She swallowed ..."
+	if(current == 0) {
 
-  if(current < number-1)                        // if we are not at the last animal, make the recursive call
-    nurseryrhyme(current+1);
-                                                // print something after the recursive call
+		printf("There was an old lady who swallowed a ");
+		printf("%s;", animals[current]);
+		nurseryrhyme(current + 1);
+
+	} else {
+
+		printf("%*s", current, "");
+		printf("She swallowed a %s", animals[current - 1]);
+		printf(" to catch a %s;", animals[current]);
+
+		if(current < number-1)
+		nurseryrhyme(current+1);
+	}
+
+	printf("%*s", current, "");
+	printf("I don't know why she swallowed a %s", animals[current]);
+	printf(" - %s", lyrics[current]);
 }
 
 
 int main() {
-  int i=0;
+	int i=0;
 
-  while (1) {
+	while (i < 20) {
+
+	fgets(animals[i], 15, stdin);
+	int q = strlen(animals[i]);
                                                 // read the next animal name
-    if (strcmp(animals[i], "END\n") == 0)       // if it is "END\n", we are done reading
-      break;
-
+	if (strcmp(animals[i], "END\n") == 0)       // if it is "END\n", we are done reading
+	break;
+	animals[i][q] = '\0';
+	fgets(lyrics[i], 60, stdin);
                                                 // determine the length of the string read
                                                 // strip the newline at the end
                                                 // read the lyric corresponding to the animal
-    i++;
-  }
+	i++;
+	}
 
-  number = i;
+	number = i;
 
-  nurseryrhyme(0);
+ 	nurseryrhyme(0);
 }
